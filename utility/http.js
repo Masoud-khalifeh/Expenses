116adf87107c747cc1e1a23b0867dd3f544f586f
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-07-22 09:33:33 +0200

    add hash password to http

--- utility/http.js
+++ utility/http.js
@@ -1,20 +1,33 @@
 import axios from 'axios';
+import * as Crypto from 'expo-crypto'; 
 
 let url = "https://uiux-expert.de/api/sqlhandle.php";
 
-async function postUser(email, name, password) {
-    try {
-        let fData = new FormData();
-        fData.append('email', email);
-        fData.append('name', name);
-        fData.append('password', password);
+async function hashPassword(password) {
+  const digest = await Crypto.digestStringAsync(
+    Crypto.CryptoDigestAlgorithm.SHA256,
+    password
+  );
 
-        const response = await axios.post(url, fData);
-        alert("The User Created Successfully");
-        return true; 
-    } catch (error) {
-        return false; 
-    }
+  return digest;
+}
+
+async function postUser(email, name, password) {
+  try {
+    
+    const hashedPassword = await hashPassword(password);
+
+    let fData = new FormData();
+    fData.append('email', email);
+    fData.append('name', name);
+    fData.append('password', hashedPassword); 
+
+    const response = await axios.post(url, fData);
+    alert("The User Created Successfully");
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 export default postUser;
